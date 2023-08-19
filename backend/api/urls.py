e725d171e2b095e92485a335606047a7a3248384
Arnold Lara <arnold.lara@gmail.com>
2023-08-19 00:32:30 -0600

    Api app created, first commit

---
+++ backend/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+from . import views
+
+app_name = "api"
+
+urlpatterns = [
+    path('auth/', obtain_auth_token),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('dj-auth/', include('dj_rest_auth.urls')),
+    path('dj-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('globals/', include('base.urls')),
+    path('', views.api_home)
+]
